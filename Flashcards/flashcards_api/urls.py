549772b531f8c6c982d070a132977828051bd61e
Jeremiah Booth <jereimah.boothphotog@gmail.com>
2021-07-01 07:14:58 -0700

    add the GET and POST for cards

--- Flashcards/flashcards_api/urls.py
+++ Flashcards/flashcards_api/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('categories/', views.CategoryList.as_view()),
-
+    path('category/<int:pk>/', views.CategoryDetail.as_view()),
+    path('cards/', views.CardsList.as_view()),
 ]
--- Flashcards/flashcards_api/views.py
+++ Flashcards/flashcards_api/views.py
@@ -11,7 +11,7 @@ class CategoryList(APIView):
     def get(self, request): 
         categories = Category.objects.all()
         serializer = CategorySerializer(categories, many=True)
-        return Response(serializer.data)\
+        return Response(serializer.data)
 
     # adding a category to the DB
     def post(self, request): 
@@ -25,17 +25,15 @@ class CategoryDetail(APIView):
 
     def get_category(self, pk):
         try:
-            return Category.object.get(pk=pk)
+            return Category.objects.get(pk=pk)
         except Category.DoesNotExist:
             raise Http404
     
     def get(self, request, pk):
         category = self.get_category(pk)
-        serializer = CategorySerializer(category, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
+        
     
     def put(self, request, pk):
         category = self.get_category(pk)
@@ -44,3 +42,20 @@ class CategoryDetail(APIView):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CardsList(APIView):
+
+    def get(self, request): 
+        categories = Cards.objects.all()
+        serializer = CardsSerializer(categories, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CardsSerializer(data=request.data)
+        if serializer.is_valid(): 
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
+
